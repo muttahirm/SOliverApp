652d9332090b33e2cf14e3490cf7b7581e7f3d8e
Muttahir Mumtaz <mumtaz.muttahir@gmail.com>
2025-04-07 19:43:17 +0200

    adds the filter screen

--- src/navigations/rootStackNavigation/rootStackNavigator.tsx
+++ src/navigations/rootStackNavigation/rootStackNavigator.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../types/rootStackParams';
-import  BottomTabNavigator  from '../bottomTabNavigation/bottomTabNavigator';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../types/rootStackParams';
+import BottomTabNavigator from '../bottomTabNavigation/bottomTabNavigator';
+import Filter from '../../screens/filters/FilterScreen';
+import ButtonBack from '../../components/Button/BackButton';
+// import {useFilters} from '../../providers/filterProviderContext';
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+// 👉 Back button component
+const HeaderLeftBackButton = ({ navigation }: { navigation: any }) => {
+  return (
+    <ButtonBack
+      onButtonPress={() => {
+        navigation.goBack();
+      }}
+    />
+  );
+};
+
+// 👉 Options config for FilterScreen
+const getFilterScreenOptions = (navigation: any) => ({
+  headerTitle: () => null,
+  headerLeft: () => <HeaderLeftBackButton navigation={navigation} />,
+});
 
 const RootStackNavigator = () => {
   return (
@@ -12,13 +32,17 @@ const RootStackNavigator = () => {
       screenOptions={{
         headerTitleAlign: 'center',
         headerBackVisible: false,
-      }}
-    >
+      }}>
       <RootStack.Group>
         <RootStack.Screen
           name="BottomTabScreen"
           component={BottomTabNavigator}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
+        />
+        <RootStack.Screen
+          name="FilterScreen"
+          component={Filter}
+          options={({navigation}) => getFilterScreenOptions(navigation)}
         />
       </RootStack.Group>
     </RootStack.Navigator>
